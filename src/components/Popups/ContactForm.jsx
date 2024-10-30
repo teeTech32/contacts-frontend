53a859d6a527b707e6e405a54ee90f9ab408322c
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2024-10-30 18:08:00 +0100

    ContactForm.jsx
    d

--- src/components/Popups/ContactForm.jsx
+++ src/components/Popups/ContactForm.jsx
@@ -6,6 +6,8 @@ import DeletIcon from "../ReactIcons/DeleteIcon"
 import {toast} from "react-toastify"
 import Spinner from "../Layouts/Spinner"
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 function ContactForm(){ 
   const navigate = useNavigate()
   const params = useParams()
@@ -43,11 +45,11 @@ function ContactForm(){
       inputDataCopy.username = username
       const formData = new FormData();
       formData.append('file', imageInput, imageInput.name)
-      await fetch('https://contacts-backend-w5bb.onrender.com/UploadImages/', {
+      await fetch('apiUrl/UploadImages/', {
         method:"POST",
         body:formData,
       })
-      const response = await fetch("https://contacts-backend-w5bb.onrender.com/contacts/", {
+      const response = await fetch("apiUrl/contacts/", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body:JSON.stringify(inputDataCopy),
