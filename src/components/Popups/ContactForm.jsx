9fe7992cfc1d919797f851eab79abe6a7d788dc6
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-14 14:12:27 +0100

    ContactForm.jsx

--- src/components/Popups/ContactForm.jsx
+++ src/components/Popups/ContactForm.jsx
@@ -1,6 +1,5 @@
 import { useContext} from "react"
 import { useParams, useNavigate} from "react-router-dom"
-import ReactModal from "react-modal"
 import contactContext from "../../contexts/ContactContext"
 import DeletIcon from "../ReactIcons/DeleteIcon"
 import {toast} from "react-toastify"
@@ -11,7 +10,7 @@ const apiUrl = process.env.REACT_APP_API_URL;
 function ContactForm(){ 
   const navigate = useNavigate()
   const params = useParams()
-  const { setAddform, addform, fetchContacts, imageInput, setImageInput, inputData, setInputData, isloading, setIsloading, contacts, setContacts, fetchUsercontacts
+  const {fetchContacts, imageInput, setImageInput, inputData, setInputData, isloading, setIsloading, contacts, setContacts, fetchUsercontacts
 } = useContext(contactContext)
   const { first_name, last_name, email, mobile_no, country_name, state_name, } = inputData
 
@@ -19,7 +18,6 @@ function ContactForm(){
   
   const handleForm = (e)=>{
     e.preventDefault()
-    setAddform(false)
     navigate(`/contact/${username}`)
   }
 
@@ -84,9 +82,9 @@ function ContactForm(){
     }) 
   }
 
-  return <ReactModal isOpen={addform} appElement={document.getElementById('root')} className="style">
+  return <div class='bg-blue-950/75 bg-opacity-50 h-screen py-20 px-5'>
           {isloading ? <Spinner/> : <div class="flex justify-center mb-0 mt-10">
-            <div class="container border-4 rounded-xl border-dashed border-blue-800 w-96 h-100 bg-gradient-to-t from-pink-800 to-blue-500 hover:shadow-2xl">
+            <div class="container border-4 rounded-xl border-dashed border-blue-800 w-92 h-100 bg-gradient-to-t from-pink-800 to-blue-500 hover:shadow-2xl">
               <div class="form">
                 <form onSubmit={handleSubmit}>
                   <div>
@@ -157,6 +155,6 @@ function ContactForm(){
             </div>
           </div>
           }
-        </ReactModal> 
+        </div> 
 }
 export default ContactForm
