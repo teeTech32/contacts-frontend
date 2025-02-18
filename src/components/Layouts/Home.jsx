3fe04928d7b935584c33e03b1451f2e94d9d447b
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-18 15:16:58 +0100

    Home.jsx

--- src/components/Layouts/Home.jsx
+++ src/components/Layouts/Home.jsx
@@ -3,7 +3,7 @@ import PopButton from './PopButton'
 import Footer from './Footer'
 
 function Home(){  
-  return <div class="bg-blue-400 hover:bg-gradient-to-r from-emerald-200 from-10% via-pink-500 via-30% to-blue-500 to-90%" >
+  return <div class="bg-blue-950/75 bg-opacity-50 hover:bg-gradient-to-r from-emerald-200 from-10% via-pink-500 via-30% to-blue-500 to-90%" >
             <MegaNavbar />
             <PopButton />
             <Footer />
