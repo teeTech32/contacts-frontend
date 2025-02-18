0d2be81b58f0fd538b0675c0869ea00751da1949
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-18 18:27:20 +0100

    Preview.jsx

--- src/components/Popups/Preview.jsx
+++ src/components/Popups/Preview.jsx
@@ -10,7 +10,8 @@ function Preview({ contact: { first_name, last_name, photo_name, mobile_no, id }
   return <div class="compact side card bg-pink-200 hover:bg-gradient-to-r from-pink-500 from-10% via-emerald-500 via-30% to-blue-500 to-90% shadow-xl hover:text-white">
             <div class="absolute inline-flex right-0 p-2 gap-1 font-bold ">
               <div onClick={()=>{
-                navigate('/contactEdit/')
+                setViewContacts(false)
+                navigate('/contact/contactEdit')
                 onEditform(id)}}  class="inline text-xl hover:bg-green-500  cursor-pointer">
                <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21.561 5.318l-2.879-2.879c-.293-.293-.677-.439-1.061-.439-.385 0-.768.146-1.061.439l-3.56 3.561h-9c-.552 0-1 .447-1 1v13c0 .553.448 1 1 1h13c.552 0 1-.447 1-1v-9l3.561-3.561c.293-.293.439-.677.439-1.061s-.146-.767-.439-1.06zm-10.061 9.354l-2.172-2.172 6.293-6.293 2.172 2.172-6.293 6.293zm-2.561-1.339l1.756 1.728-1.695-.061-.061-1.667zm7.061 5.667h-11v-11h6l-3.18 3.18c-.293.293-.478.812-.629 1.289-.16.5-.191 1.056-.191 1.47v3.061h3.061c.414 0 1.108-.1 1.571-.29.464-.19.896-.347 1.188-.64l3.18-3.07v6zm2.5-11.328l-2.172-2.172 1.293-1.293 2.171 2.172-1.292 1.293z"></path></svg>
               </div>
