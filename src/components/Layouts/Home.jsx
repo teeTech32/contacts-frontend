e4503bf83fb484b058f6197742e0e5ecd654f6c7
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-18 15:20:29 +0100

    Home.jsx

--- src/components/Layouts/Home.jsx
+++ src/components/Layouts/Home.jsx
@@ -6,7 +6,6 @@ function Home(){
   return <div class="bg-blue-950/75 bg-opacity-50 hover:bg-gradient-to-r from-emerald-200 from-10% via-pink-500 via-30% to-blue-500 to-90%" >
             <MegaNavbar />
             <PopButton />
-            <Footer />
           </div>
 }
 export default Home
