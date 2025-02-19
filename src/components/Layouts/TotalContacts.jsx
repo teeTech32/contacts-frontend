770ef6cda6d4017ed964fc35a6bc8347b47107c0
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-19 15:47:22 +0100

    Update TotalContacts.jsx

--- src/components/Layouts/TotalContacts.jsx
+++ src/components/Layouts/TotalContacts.jsx
@@ -5,7 +5,11 @@ function TotalContacts(){
   const {contacts} = useContext(contactContext)
 
   if(contacts.length!==0){
-    return <div class="p-5">
+    return <div class="p-5" data-aos='fade-right'
+                            data-aos-offset='300'
+                            data-aos-delay='300'
+                            data-aos-duration='3000'
+                            data-aos-easing='ease-in-sine'>
              <div class="m-5 hover:text-white h-10 w-52 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
                 <div class="text-black-800 font-bold ">
                   <h1 class="pt-2 pl-3" >
@@ -15,7 +19,11 @@ function TotalContacts(){
               </div>
             </div> 
   }
-  return <div class="p-5">
+  return <div class="p-5" data-aos='fade-right'
+                          data-aos-offset='300'
+                          data-aos-delay='300'
+                          data-aos-duration='3000'
+                          data-aos-easing='ease-in-sine'>
           <div class="m-5 hover:text-white h-10 w-56 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
             <div class="text-black-800 font-bold text-md">
               <h1 class="pt-2 pl-1">
