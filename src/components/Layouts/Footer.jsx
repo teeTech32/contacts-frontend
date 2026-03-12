227791615a1df6331ee858c9870a48bbcbe7d888
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2026-03-12 13:19:52 +0100

    Footer.jsx

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -4,7 +4,7 @@ import SmileFace from '../ReactIcons/SmileFace'
 function Footer() {
   const footerYear = new Date().getFullYear();
   return (
-    <footer class='footer-center footer text-content-primary bg-blue-950/85 bg-opacity-50 p-5'>
+    <footer class='footer-center mb-none footer text-content-primary bg-blue-950/85 bg-opacity-50 p-5'>
       <div>
         <SmileFace class='text-5xl text-white ' />
         <p class='text-white text-sm font-bold'>teeTech&copy;{footerYear} All copy rights reserved!</p>
