0f2fa73dc9e2cfa920c2be4e7660bc2eea8a3ba9
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2026-05-02 16:11:09 +0100

    TotalContacts.jsx

--- src/components/Layouts/TotalContacts.jsx
+++ src/components/Layouts/TotalContacts.jsx
@@ -10,7 +10,7 @@ function TotalContacts(){
                             data-aos-delay='300'
                             data-aos-duration='3000'
                             data-aos-easing='ease-in-sine'>
-             <div class="m-5 hover:text-white h-10 w-52 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
+             <div class="m-5 text-black hover:text-white h-10 w-52 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
                 <div class="text-black-800 font-bold ">
                   <h1 class="pt-2 pl-3" >
                     Total Contact : {contacts.length}
@@ -24,7 +24,7 @@ function TotalContacts(){
                           data-aos-delay='300'
                           data-aos-duration='3000'
                           data-aos-easing='ease-in-sine'>
-          <div class="m-5 hover:text-white h-10 w-56 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
+          <div class="m-5 text-black hover:text-white h-10 w-56 bg-pink-300 rounded hover:bg-blue-500 shadow-xl">
             <div class="text-black-800 font-bold text-md">
               <h1 class="pt-2 pl-1">
                 No Contact is available yet!
