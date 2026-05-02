845c3232b7d0e5f87f60f9c4916b96b649544656
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2026-05-02 19:49:27 +0100

    ContactForm.jsx

--- src/components/Popups/ContactForm.jsx
+++ src/components/Popups/ContactForm.jsx
@@ -100,23 +100,23 @@ function ContactForm(){
                   <div class="mr-5  mb-5 mt-5">
                     <div class="inline-flex justify-between ml-2 gap-8 md:gap-12">
                       <label htmlFor="FirstName" class="form-title label text-md font-bold  text-black">FirstName</label>
-                      <input id="first_name" value={first_name} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0  ml-19 hover:bg-blue-200 text-black" placeholder="First_Name" type="text" required />
+                      <input id="first_name" value={first_name} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0  ml-19 bg-white hover:bg-blue-200 text-black" placeholder="First_Name" type="text" required />
                     </div>
                     <div class="inline-flex justify-between mt-2 p-2 gap-8 md:gap-12">
                       <label htmlFor="LastName" class="form-title label text-md font-bold  text-black">LastName</label>
-                      <input id="last_name" value={last_name} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 hover:bg-blue-200 text-black" placeholder="Last_Name" type="text" required />
+                      <input id="last_name" value={last_name} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 bg-white hover:bg-blue-200 text-black" placeholder="Last_Name" type="text" required />
                     </div>
                     <div class="inline-flex justify-between p-2 gap-2 md:gap-5">
                       <label htmlFor="EmailAddress" class="form-title label text-md font-bold  text-black">EmailAddress</label>
-                      <input id="email" value={email} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 hover:bg-blue-200 text-black" placeholder="Email_Address" type="email" required />
+                      <input id="email" value={email} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 bg-white hover:bg-blue-200 text-black" placeholder="Email_Address" type="email" required />
                     </div>
                     <div class="inline-flex justify-between p-2 gap-0 md:gap-3">
                       <label htmlFor="MobileContact" class="form-title label text-md font-bold  text-black">MobileContact</label>
-                      <input id="mobile_no" value={mobile_no} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 hover:bg-blue-200 text-black" placeholder="Phone_Number" type="text" required />
+                      <input id="mobile_no" value={mobile_no} onChange={handleOnchange} class="input input-sm w-sm pr-30 mr-0 ml-19 bg-white hover:bg-blue-200 text-black" placeholder="Phone_Number" type="text" required />
                     </div>
                     <div class="inline-flex justify-between pl-2 gap-12">
                       <label htmlFor="CountryN" class="form-title label text-md font-bold  text-black">Country</label>
-                      <select id="country_name" value={country_name} onChange={handleOnchange} class="input input-sm w-[200px] pr-30 ml-19 hover:bg-blue-200 text-black" placeholder="Country_Name" type="multiple" required >
+                      <select id="country_name" value={country_name} onChange={handleOnchange} class="input input-sm w-[200px] pr-30 ml-19 bg-white hover:bg-blue-200 text-black" placeholder="Country_Name" type="multiple" required >
                         <option value=""></option>
                         <option value="Nigeria">Nigeria</option>
                         <option value="United K">United K</option>
@@ -126,7 +126,7 @@ function ContactForm(){
                     </div>
                     <div class="inline-flex  p-3 gap-16">
                       <label htmlFor="StateName" class="form-title label text-md font-bold  text-black">State</label>
-                      <select id="state_name" value={state_name} onChange={handleOnchange} class="input input-sm w-[200px] pr-30 mr-0 ml-19 hover:bg-blue-200 text-black" placeholder="State_Name" type="multiple" required>
+                      <select id="state_name" value={state_name} onChange={handleOnchange} class="input input-sm w-[200px] pr-30 mr-0 ml-19 bg-white hover:bg-blue-200 text-black" placeholder="State_Name" type="multiple" required>
                         <option value=""></option>
                         <option value="Delta">Delta</option>
                         <option value="Ogun">Ogun</option>
@@ -144,17 +144,17 @@ function ContactForm(){
                     </div>
                     <div class="inline-flex justify-between pl-2 gap-14">
                       <label htmlFor="Picture" class="form-title label text-md font-bold  text-black">Picture</label>
-                      <input id="files" class="input input-sm w-[200px] pr-0 mr-0 ml-19 hover:bg-blue-200 text-black" type="file" 
+                      <input id="files" class="input input-sm w-[200px] pr-0 mr-0 ml-19 bg-white hover:bg-blue-200 text-black" type="file" 
                       accept='.jpg,.png,.jpeg' required onChange={handleImage} />
                     </div>
                   </div>
                   <div class="flex inlineflex mb-5  float-right mr-5 ">     
-                    <button type="submit" class="btn btn-sm hover:bg-gradient-to-l from-green-500 via-orange-500 to-blue-500 font-bold text-black hover:text-white align-middle rounded-full">
+                    <button type="submit" class="btn btn-sm bg-white hover:bg-gradient-to-l from-green-500 via-orange-500 to-blue-500 font-bold text-black hover:text-white align-middle rounded-full">
                       Add Contact
                     </button>
                   </div>
                 </form>
-                <button type="submit" onClick={clearDetails} class="btn btn-sm ml-5  hover:bg-gradient-to-l from-green-500 via-orange-500 to-blue-500 text-black font-bold hover:text-white inline rounded-full" >
+                <button type="submit" onClick={clearDetails} class="btn btn-sm ml-5 bg-white  hover:bg-gradient-to-l from-green-500 via-orange-500 to-blue-500 text-black font-bold hover:text-white inline rounded-full" >
                   Cancel
                 </button>
               </div>
